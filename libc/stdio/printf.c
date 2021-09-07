7a62845295fb3019ae2a44367664d3aae679a051
kiran-user <89889868+kiran-user@users.noreply.github.com>
2021-09-07 12:18:12 +0530

    Update printf.c

--- libc/stdio/printf.c
+++ libc/stdio/printf.c
@@ -12,7 +12,7 @@ static bool print(const char* data, size_t length) {
 			return false;
 	return true;
 }
-<!--printf file is edited to check auto triggering-->
+<!--printf file is edited to check auto triggering or not-->
 int printf(const char* restrict format, ...) {
 	va_list parameters;
 	va_start(parameters, format);
